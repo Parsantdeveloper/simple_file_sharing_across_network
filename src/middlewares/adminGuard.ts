7c059b2f14bd6216425cf71e1392c5f56e8ebf4f
Prashant Bhusal <parshantbhusal1928@gmail.com>
2026-03-23 21:15:38 +0545

    deleted unused files

--- src/middlewares/adminGuard.ts
+++
@@ -1,7 +0,0 @@
-import { Request,Response,NextFunction } from "express";
-export function requireAdmin(req:Request, res:Response, next:NextFunction) {
-  if (req.user.role !== "ADMIN") {
-    return res.status(403).json({ message: "Admin only" });
-  }
-  next();
-}
